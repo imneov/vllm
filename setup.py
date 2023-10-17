fe2c4d486a06f6a719d659be334a30308b505e94
neolf <neolf.box@gmail.com>
2023-10-17 09:41:47 +0800

    feat: Skip Device Capability

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ if not compute_capabilities:
     device_count = torch.cuda.device_count()
     for i in range(device_count):
         major, minor = torch.cuda.get_device_capability(i)
-        if major < 7:
+        if major < 6: # Support Device Capability 6 (eg. GeForce GTX 1080)
             raise RuntimeError(
                 "GPUs with compute capability below 7.0 are not supported.")
         compute_capabilities.add(f"{major}.{minor}")
